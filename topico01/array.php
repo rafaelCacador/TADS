05106a8a36f47e334193bd27d4a0e8d87eb77c81
Rafael Caçador Dioscânio <141187927+rafaelCacador@users.noreply.github.com>
2024-02-27 21:50:01 -0300

    Add files via upload

---
+++ topico01/array.php
@@ -0,0 +1,26 @@
+<?php
+    //php.net
+
+    $precos = array(1.56,5.74,0.23,8.96);
+
+    var_dump($precos);
+    var_dump($precos[2]);
+     
+    $precos = array(2 =>1.56,0 => 5.74,1 => 0.23,8.96);
+
+    
+    var_dump($precos);
+    var_dump($precos[2]);
+
+/*$precos = array();
+
+$precos[0] = 1.75;
+$precos[32] = 4.65;
+$precos[2] = 7.89;
+$precos[] = 1.32; 
+$precos[12] = 14;
+$precos[] = 7.89; 
+
+var_dump($precos);
+*/
+?>
---
+++ topico01/hello.php
@@ -0,0 +1,5 @@
+<?php
+    echo "Faculdade <b>Vianna Jr</b> <br>";
+    echo "Disciplina de: \n";
+    echo "Linguagem de Programação";
+?>
