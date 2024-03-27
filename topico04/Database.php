f9d19bef30ff3cd3de72df8a6ca571547a9bd0fd
Rafael Cacador <rafael.dioscanio@viannasempre.com.br>
2024-03-26 23:21:00 -0300

    Adicionando Database e formulário

---
+++ topico04/Database.php
@@ -0,0 +1,48 @@
+<?php
+
+    class Database{
+
+        static $db = null;
+
+        public function __construct() {
+        
+            if (self::$db == null){
+                self::$db = new mysqli('mariadb','root','root','tads');
+        
+                if ( self::$db->connect_errno > 0){
+                    die("Ocorreu um erro { self::$db->connect_errno}");
+                }
+            }
+        
+        }
+    
+       function getConnection(){
+            return self::$db;
+        }
+        function closeConnection(){
+            self::$db->close();
+        }
+
+}
+
+
+    //localhost:8081
+
+    //$db = new mysqli('mariadb', 'root', 'root', 'tads');  
+
+    //$query = 'SELECT * FROM tads.alunos';
+
+    //$result = $db->query($query);
+
+    //while($linha = $result->fetch_assoc()){ //pega os resultados pelo array associativo
+    //    echo "<p> {$linha["id"]} {$linha["matricula"]} {$linha["nome"]} </p>";
+    //}
+
+    //var_dump($db);
+
+    //$db->close();
+ 
+
+   
+
+
---
+++ topico04/recado.php
@@ -0,0 +1,25 @@
+<form action="" method="post">
+    Nome:
+    <input type="text" name="nome" id="nome"><br>
+    Email:
+    <input type="text" name="email" id="email"><br>
+    Cidade:
+    <input type="text" name="cidade" id="cidade"><br>
+    Recado:
+    <textarea name="recado" id="recado" cols="30" rows="10"></textarea><br>
+    <input type="submit" value="Cadastrar" name="cadastrar">
+</form>
+
+<?php
+
+    if(isset($_POST["cadastrar"])):
+        $nome = $_POST["nome"];
+        $email = $_POST["email"];
+        $cidade =$_POST["cidade"];
+        $texto = $_POST["recado"];
+
+        $sql = "INSERT INTO tads.recados
+        (nome, email, cidade, texto)
+        VALUES('{$nome}','{$email}','{$cidade}','{$texto}');";
+
+    endif;  
